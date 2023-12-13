07d019b83ada7b3ba13656cf59e51ffdf3a29656
ChaerulHidayat17 <chaerulhidayat70@gmail.com>
2023-12-13 23:03:51 +0700

    Update

--- app/src/main/java/com/helloworld/MainActivity.java
+++ app/src/main/java/com/helloworld/MainActivity.java
@@ -1,15 +1,63 @@
 package com.helloworld;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.helloworld.R;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout frameLayout;
+    TabLayout tabLayout;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.framelayout);
+        tabLayout = findViewById(R.id.tablayout);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, new ActionFragment())
+                .addToBackStack(null)
+                .commit();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment fragment = null;
+                switch (tab.getPosition()) {
+                    case 0:
+                        fragment = new ActionFragment();
+                        break;
+                    case 1:
+                        fragment = new ComedyFragment();
+                        break;
+                    case 2:
+                        fragment = new RomanceFragment();
+                        break;
+                }
+
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, fragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
